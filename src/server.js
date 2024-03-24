08022032272bc991fd61453921f3b947dcef6c29
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-03-24 19:50:08 -0300

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/', routes);
 
